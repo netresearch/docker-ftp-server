23a98a1424b4f6ba1e529d8fa687e690894b9249
Torsten Fink <torsten.fink@netresearch.de>
2018-04-24 08:19:45 +0200

    BBSUP-1433: Set all permissions only for ftp-user on his homefolder
    Prevent other ftp users to access anything in other users ftp folders

--- setup/docker-entrypoint.sh
+++ setup/docker-entrypoint.sh
@@ -8,6 +8,8 @@ for userAndPass in "${arrUSERS[@]}"
         adduser -h /home/./${username} -s /bin/false -D ${username}
         echo "${userAndPass}" | /usr/sbin/chpasswd
         chown ${username}:${username} /home/${username}/ -R
+        find /home/${username} -type d -exec chmod 2700 {} \;
+        find /home/${username} -type f -exec chmod 0600 {} \;
 done
 
 /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
