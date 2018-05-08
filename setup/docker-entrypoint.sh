18f8ce26e9bd387cd1142c3c86b246c0b8255836
Axel Kummer <axel.kummer@netresearch.de>
2018-05-08 10:20:20 +0200

    SRVFTP-1: check if user already exists, fix permissions of config file

--- setup/docker-entrypoint.sh
+++ setup/docker-entrypoint.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 set -e
 
+# Add Users and Home directories
 arrUSERS=(${USERS//;/ })
 for userAndPass in "${arrUSERS[@]}"
     do
         username=${userAndPass%%:*}
-        adduser -h /home/./${username} -s /bin/false -D ${username}
+        # Check if user exists and create if not
+        userInfo="$(id ${username} || echo 'no such user')"
+        if [ "${userInfo}" == "no such user" ]; then
+            adduser -h /home/./${username} -s /bin/false -D ${username}
+            echo "Create user ${username}"
+        fi
         echo "${userAndPass}" | /usr/sbin/chpasswd
         chown ${username}:${username} /home/${username}/ -R
         find /home/${username} -type d -exec chmod 2700 {} \;
         find /home/${username} -type f -exec chmod 0600 {} \;
 done
+
+# Fix vsftpd.conf permissions
+chown root /etc/vsftpd/vsftpd.conf
+chmod 664  /etc/vsftpd/vsftpd.conf
 
 "$@"
