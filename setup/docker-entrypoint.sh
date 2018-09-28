7f1781b6f754fde8d1405a8c68782c78d9a975f7
Axel Seemann <axel.seemann@netresearch.de>
2018-09-28 16:33:35 +0200

    OPSG-49: Enable logging in ftp container
    * make configuration possible via environment variables
    * add config options for passive mode

--- setup/docker-entrypoint.sh
+++ setup/docker-entrypoint.sh
@@ -28,4 +28,30 @@ done
 chown root /etc/vsftpd/vsftpd.conf
 chmod 664  /etc/vsftpd/vsftpd.conf
 
+
+if [ "${ENABLE_LOGGING}" == "yes" ]; then
+    echo "xferlog_enable=YES" >> /etc/vsftpd/vsftpd.conf
+fi
+
+if [ "${ENABLE_PASSIVE_MODE}" == "yes" ]; then
+    echo "pasv_enable=YES" >> /etc/vsftpd/vsftpd.conf
+    echo "port_enable=YES" >> /etc/vsftpd/vsftpd.conf
+fi
+
+if [ ! -z ${PASSIVE_MODE_MIN_PORT} ]; then
+    echo "pasv_min_port=${PASSIVE_MODE_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
+fi
+
+if [ ! -z ${PASSIVE_MODE_MAX_PORT} ]; then
+    echo "pasv_max_port=${PASSIVE_MODE_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
+fi
+
+if [ ! -z ${PASSIVE_MODE_ADDRESS} ]; then
+    echo "pasv_address=${PASSIVE_MODE_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
+fi
+
+if [ "${PASSIVE_MODE_RESOLVE}" == "yes" ]; then
+    echo "pasv_addr_resolve=YES" >> /etc/vsftpd/vsftpd.conf
+fi
+
 "$@"
